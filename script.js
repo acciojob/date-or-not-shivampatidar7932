ccbde3846f24b5ccaaf90f885a6fc0be105e99e1
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-11 09:30:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 var isDate = function (input) {
   //   write your code here
+	if (input instanceof Date) {
+    return true;
+  }
+
+  // Check if the input is a valid date string
+  if (typeof input === 'string') {
+    const date = new Date(input);
+    return !isNaN(date.getTime());
+  }
+	if (typeof input === 'number' && !isNaN(input)) {
+    const date = new Date(input);
+    return !isNaN(date.getTime());
+  }
+
+  // If none of the conditions are met, return false
+  return false;
 };
 
 // Do not change the code below.
